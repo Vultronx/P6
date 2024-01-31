bd7045a13c4f9491cec95c06b6431c0c6dedac46
Sylteck <gaetan.gobin@free.fr>
2024-01-31 13:00:06 +0100

    Update Error/index.js

--- kasa/src/pages/Error/index.js
+++ kasa/src/pages/Error/index.js
@@ -4,7 +4,7 @@ import '../../styles/App.scss';
 function Error() {
   return (
     <div className="Error">
-        <main className="App-body">
+        <main className="App-body Error">
             <span className="App-color App-font-size-xxl .App-font-weight-700 App-margin-default">404</span>
             <p className="App-color App-font-size-s">Oups! La page que vous demandez n'existe pas.</p>
             <Link className='App-link App-font-size-xs App-margin-default' to="/">Retourner sur la page d’accueil</Link>
